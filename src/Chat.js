2bb3075fa739f3c5ff566ccf3082c7c6485e24fb
mike <michael.ingley@gmail.com>
2020-11-09 13:11:41 -0800

    init

---
+++ src/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Chat.css";
+
+function Chat() {
+    return (
+        <div className="chat">
+            chat comp
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/Sidebar.js
@@ -0,0 +1,26 @@
+import { Avatar, IconButton } from '@material-ui/core'
+import { Chat, DonutLarge, MoreVert } from '@material-ui/icons'
+import React from 'react'
+import './Sidebar.css'
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <Avatar src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.KlnaM8eoWZhWkHGTHhb4xQAAAA%26pid%3DApi%26h%3D160&f=1"/>
+                <div className="sidebar__headerRight"></div>
+                <IconButton>
+                    <DonutLarge />
+                </IconButton>
+                <IconButton>
+                    <Chat />
+                </IconButton>
+                <IconButton>
+                    <MoreVert />
+                </IconButton>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
